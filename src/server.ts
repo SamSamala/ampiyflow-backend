d63853a58e778a667da02f58213a7df8a071a8d3
SamSamala <118750194+SamSamala@users.noreply.github.com>
2026-02-21 14:12:58 +0530

    clean gitignore and production setup

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+  "http://localhost:3000",
+  "https://ampiyflow.com",
+],
     credentials: true,
   })
 );
